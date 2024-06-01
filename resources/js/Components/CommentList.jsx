3bfcafb8795e9c5cee3614c36673334c0e040af5
RogerCiv <rogercivdev@gmail.com>
2024-06-01 20:13:59 +0200

    CommentList.jsx. Change like icons

--- resources/js/Components/CommentList.jsx
+++ resources/js/Components/CommentList.jsx
@@ -2,12 +2,23 @@ import React, { useState, useRef, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import dayjs from "dayjs";
-import { useForm, usePage } from "@inertiajs/react";
+import { useForm, usePage,Link } from "@inertiajs/react";
 import Dropdown from "@/Components/Dropdown";
 import PrimaryButton from "@/Components/PrimaryButton";
 import InputError from "@/Components/InputError";
 import TextInput from "./TextInput";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import {faHeart as faHeartRegular} from "@fortawesome/free-regular-svg-icons";
+import Avatar from "@mui/material/Avatar";
 
+const MyComponent = () => {
+    return (
+        <div>
+            <FontAwesomeIcon icon={faHeartRegular} className="mr-2 text-primary-600" />
+            <FontAwesomeIcon icon={faHeartSolid} className="mr-2 text-text-950" />
+        </div>
+    );
+};
 const CommentList = ({ comments,likedComments, handleLikeComment, handleUnlikeComment }) => {
     const { auth } = usePage().props;
     const [editingCommentId, setEditingCommentId] = useState(null);
@@ -48,21 +59,32 @@ const CommentList = ({ comments,likedComments, handleLikeComment, handleUnlikeCo
                 const isLiked = comment.likes.some(like => like.id === auth.user.id);
 
                 return (
-                    <div key={comment.id} className="border-t border-gray-200 pt-4 flex items-start space-x-4">
-                        <img className="rounded-full w-10 h-10" src={comment.user.avatar ? `/storage/${comment.user.avatar}` : '/img/avatar_default.jpg'} alt={comment.user.name} />
+                    <div key={comment.id} className="border-t border-accent-400 pt-4 flex items-start space-x-4 mb-2">
+                        {/*<img className="rounded-full w-10 h-10" src={comment.user.avatar ? `/storage/${comment.user.avatar}` : '/img/avatar_default.jpg'} alt={comment.user.name} />*/}
+                        <Link href={route('profile', {name: comment.user.name})}>
+                        <Avatar
+                            alt={`${comment.user.name} Avatar`}
+                            src={comment.user.avatar ? `/storage/${comment.user.avatar}` : "/img/avatar_default.jpg"}
+                            sx={{ width: 45, height: 45 }}
+                            className='border border-2 border-accent-500 hover:border-accent-300'
+                        />
+                        </Link>
                         <div className="w-full">
                             <div className="flex items-center space-x-2">
-                                <h5 className="text-sm font-bold">{comment.user.name}</h5>
-                                <small className="text-sm text-gray-600">{dayjs(comment.created_at).fromNow()}</small>
+                                <Link href={route('profile', {name: comment.user.name})}>
+
+                                <h5 className="text-sm font-bold text-text-900">{comment.user.name}</h5>
+                                <small className="text-sm text-text-600">{dayjs(comment.created_at).fromNow()}</small>
+                                </Link>
                                 {isAuthor && (
                                     <Dropdown>
                                         <Dropdown.Trigger>
-                                            <button className="text-gray-500">
+                                            <button className="text-primary-500">
                                                 <FontAwesomeIcon icon={faEllipsisV} />
                                             </button>
                                         </Dropdown.Trigger>
                                         <Dropdown.Content>
-                                            <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => startEditing(comment)}>
+                                            <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-text-950 hover:bg-background-100 focus:bg-background-150 transition duration-150 ease-in-out" onClick={() => startEditing(comment)}>
                                                 Edit
                                             </button>
                                             <Dropdown.Link as="button" href={route("comments.destroy", comment.id)} method="delete">
@@ -85,7 +107,7 @@ const CommentList = ({ comments,likedComments, handleLikeComment, handleUnlikeCo
                                 </form>
                             ) : (
                                 <div>
-                                    <p className="mt-2 text-gray-900">{comment.content}</p>
+                                    <p className="mt-2 text-text-950">{comment.content}</p>
                                     {comment.image && (
                                         <img className="rounded-lg object-cover mt-2" src={comment.image.startsWith("http") ? comment.image : `/storage/${comment.image}`} alt="Comment Image" />
                                     )}
@@ -97,8 +119,17 @@ const CommentList = ({ comments,likedComments, handleLikeComment, handleUnlikeCo
                                     className="flex items-center"
                                     onClick={(e) => isLiked ? handleUnlikeComment(comment.id, e) : handleLikeComment(comment.id, e)}
                                 >
-                                    <FontAwesomeIcon icon={faThumbsUp} className="mr-2" />
-                                    {isLiked ? 'Unlike' : 'Like'} {comment.likes.length}
+                                    {
+                                        isLiked ? (
+                                                <FontAwesomeIcon icon={faHeart} className="mr-2 text-primary-600" />
+
+                                        ) : (
+                                            <FontAwesomeIcon icon={faHeartRegular} className="mr-2 text-text-950" />
+                                        )
+                                    }
+                                    {/*<FontAwesomeIcon icon={faThumbsUp} className="mr-2" />*/}
+                                    <p className='text-text-600 text-base font-semibold'>{comment.likes.length}</p>
+
                                 </button>
                             </div>
                         </div>
